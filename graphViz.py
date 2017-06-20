b619f23ca94f2457a292741a51e21f2f3234076c
Robert Lesser <bclesser@gmail.com>
2017-06-20 01:56:07 -0400

    added wordChange file and word dictionaries

--- graphViz.py
+++ graphViz.py
@@ -5,16 +5,20 @@ from hmScrape import HypeMNetwork
 from tmdScrape import TMDNetwork
 from collatzGraph import CollatzGraph
 from ghScrape import GithubNetwork
+from wordGraph import WordChange
 
-ns = GithubNetwork()
-ns.propegateGraph('RLesser', save_interval_and_location = [100, 'github_1000'], limit = 1000, verbose = True)
-# ns.propegateGraph('BobLesser', save_interval_and_location = [500, 'hypeM_all'], verbose = True)
+ns = WordChange()
+#ns.propegateGraph('RLesser', save_interval_and_location = [100, 'github_1000'], limit = 1000, verbose = True)
+ns.propegateGraph('farce', verbose = True)
 # 'hypeM_all' killed after 18114 by /danivachon/ -> /Radio/
 
 # ns.propegateGraph(10003215, verbose = True)
-ns.saveGraph('github_1000')
-ns.loadGraph('github_1000')
+# ns.saveGraph('github_1000')
+# ns.loadGraph('github_1000')
 # ns.colorNodes(keyProperty = "customName")
 # ns.graphNetworkx(buds_visible = True, labels_visible = False, iterations = 100)
 # time.sleep(5)
+
+ns.colorNodes(keyProperty = "vowels")
+
 ns.graphD3(buds_visible = False, filter_assym_edges = True)
--- wordGraph.py
+++ wordGraph.py
@@ -2,27 +2,40 @@
 
 from networkScrape import NetworkScraper
 
-class wordChange(NetworkScraper):
-	"""docstring for wordChange"""
+class WordChange(NetworkScraper):
+	"""docstring for WordChange"""
 	def __init__(self):
-		super(wordChange, self).__init__()
+		super(WordChange, self).__init__()
 
 	def getWordsOfLength(self, length):
-		if hasattr(self, wordList):
-			return self.wordList
-		else:
-			with open("/usr/share/dict/web2", "rb") as f:
-				self.wordList = [x for x in f if len(x) == length]
+		if not hasattr(self, 'wordList'):
+			with open("engWordList.txt", "rb") as f:
+				self.wordList = [x.strip().lower() for x in f if len(x.strip()) == length]
+				print len(self.wordList)
 
 	def getDataSource(self, nodeId):
+		self.getWordsOfLength(len(nodeId))
 		data = nodeId
 		return data
 
 	def getEdgeData(self, data):
 		currentWord = data
 		adjacentWords = []
-		for word in wordList:
+		for word in self.wordList:
 			if len([i for i, j in zip(word, currentWord) if i == j]) == len(word)-1:
 				adjacentWords.append(word)
+		print adjacentWords
 		adjacentEdges = [self.makeEdgeObject(x) for x in adjacentWords]
 		return adjacentEdges
+
+	def getNodeProperties(self, data):
+		propertiesObj = {}
+		word = data
+		vowels = [l for l in word if l in "aeiou"]
+		propertiesObj["vowels"] = "".join(sorted(vowels))
+
+		return propertiesObj
+
+
+
+
